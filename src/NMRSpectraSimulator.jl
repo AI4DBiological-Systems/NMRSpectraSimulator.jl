bef7b999ff35d351c1be0dd401f119f46935b7a5
Roy <rwang013@uottawa.ca>
2022-03-17 15:46:39 -0400

    update

--- src/NMRSpectraSimulator.jl
+++ src/NMRSpectraSimulator.jl
@@ -25,6 +25,7 @@ include("../src/SH/resonance_partitioning.jl")
 include("../src/conversions/cs_parse.jl")
 
 include("../src/model_evals/lorentzian.jl")
+include("../src/model_evals/lorentzian_z.jl")
 
 
 #include("../src/library/database_helpers.jl")
